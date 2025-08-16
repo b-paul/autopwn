0960b7c28ef2d6e436d92bd7ed31d6182a5996aa
Max Guppy <theonly@mrcat.au>
2025-08-16 11:04:43 +1000

    Add radare2 afl and axt to Binary class

--- binary.py
+++ binary.py
@@ -1,4 +1,6 @@
 import cle
+import json
+import r2pipe
 from pwn import ELF
 
 class Binary():
@@ -13,3 +15,10 @@ class Binary():
         self.canary = elf.canary
         self.nx = elf.nx
         self.pie = elf.pie
+
+        self.r2 = r2pipe.open(path)
+        self.r2.cmd("aaa")
+        self.afl = json.loads(self.r2.cmd("aflj"))
+
+    def crossrefs(self, symbol: int):
+        return json.loads(self.r2.cmd(f"axtj {symbol}"))
--- goals.py
+++ goals.py
@@ -24,12 +24,21 @@ def print_goals(goals: list[Goal]):
 
 
 def find_goals(bin: Binary) -> list[Goal]:
-    ret = []
+    return find_win_functions(bin)
 
+
+def find_win_functions(bin: Binary) -> list[Goal]:
+    found_goals = []
+
+    # First, try a list of well-known goal function names
     for name in ['win', 'goal', 'wins']:
         sym = bin.loader.find_symbol(name)
         if sym is not None:
             print(sym.rebased_addr)
-            ret.append(WinFunction(name, sym.rebased_addr))
+            found_goals.append(WinFunction(name, sym.rebased_addr))
 
-    return ret
+    # Otherwise, try to detect them from their behaviour
+    print(bin.afl)
+    print(bin.crossrefs(bin.afl[0]["offset"]))
+
+    return found_goals
