4f06321d88fc91b55a4ba6ccd8c33f1b97e2201f
Max Guppy <theonly@mrcat.au>
2025-08-16 12:39:17 +1000

    Add system() call detection and goal

--- goals.py
+++ goals.py
@@ -8,10 +8,17 @@ class Goal():
     Abstract class for a possible goal.
     """
 
+
 @dataclass
 class WinFunction(Goal):
     """A function that might solve the challenge when called"""
     name: int
+    addr: int
+
+
+@dataclass
+class SystemFunction(Goal):
+    """A system() entry in the PLT"""
     addr: int
 
 
@@ -20,6 +27,10 @@ def print_goals(goals: list[Goal]):
         match goal:
             case WinFunction():
                 print(f"Win Function: {goal.name} @ 0x{goal.addr:x}")
+            case SystemFunction():
+                print(f"system() PLT entry @ 0x{goal.addr:x}")
+            case _:
+                raise ValueError("Goal is not a Goal!")
 
 
 def find_goals(bin: Binary) -> list[Goal]:
@@ -41,6 +52,25 @@ def find_win_functions(bin: Binary) -> list[Goal]:
     fopen = next((fn for fn in bin.afl if fn["name"] == "sym.imp.fopen"), None)
     if fopen:
         crossrefs = bin.crossrefs(fopen["offset"])
+        for crossref in crossrefs:
+            caller = next(fn for fn in bin.afl if fn["name"] == crossref["fcn_name"])
+            options = angr.options.unicorn
+            check = bin.angr.factory.call_state(
+                bin.angr.loader.find_symbol(caller["name"].removeprefix("sym.")).rebased_addr,
+                add_options=options
+            )
+
+            simgr = bin.angr.factory.simulation_manager(check)
+            simgr.explore(find=crossref["from"], avoid=[])
+
+            strings = set([bin.load_string(found.solver.eval(found.regs.rdi)) for found in simgr.found])
+            if any("flag.txt" in string for string in strings):
+                found_goals.append(WinFunction(caller["name"].removeprefix("sym."), caller["offset"]))
+
+    system = next((fn for fn in bin.afl if fn["name"] == "sym.imp.system"), None)
+    if system:
+        found_goals.append(SystemFunction(system["offset"]))
+        crossrefs = bin.crossrefs(system["offset"])
         for crossref in crossrefs:
             caller = next(fn for fn in bin.afl if fn["name"] == crossref["fcn_name"])
             options = angr.options.unicorn
