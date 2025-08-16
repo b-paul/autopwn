5a3218c8c0aecd52ca0951fa3b10f52d0136624b
bpaul <bpaul@bpaul.xyz>
2025-08-16 11:55:18 +1000

    Detect fgets calls

--- vulns.py
+++ vulns.py
@@ -18,7 +18,7 @@ class StackBufferOverflow(Vulnerability):
 
 
 def find_gets_vulns(bin: Binary) -> list[Vulnerability]:
-    """Find calls to gets with a stack buffer"""
+    """Find calls to gets into a stack buffer"""
 
     crossrefs = []
     for d in bin.afl:
@@ -36,10 +36,10 @@ def find_gets_vulns(bin: Binary) -> list[Vulnerability]:
         simgr = bin.angr.factory.simulation_manager(state)
         simgr.explore(find=goal)
         for found in simgr.found:
-            rbp = found.solver.eval(found.regs.get("rdi"), cast_to=int)
-            rdi = found.solver.eval(found.regs.get("rbp"), cast_to=int)
+            rdi = found.solver.eval(found.regs.get("rdi"), cast_to=int)
+            rbp = found.solver.eval(found.regs.get("rbp"), cast_to=int)
 
-            rip_offset = rdi - rbp + 8
+            rip_offset = rbp - rdi + 8
 
             # rdi-rbp is the number of bytes to write from the buffer to the end of the stack frame,
             # 8 past that is the return address
@@ -49,9 +49,43 @@ def find_gets_vulns(bin: Binary) -> list[Vulnerability]:
     return ret
 
 
+def find_fgets_vulns(bin: Binary) -> list[Vulnerability]:
+    """Find calls to fgets into a stack buffer which writes past the buffer size"""
+
+    crossrefs = []
+    for d in bin.afl:
+        if d["name"] == "sym.imp.fgets":
+            crossrefs = bin.crossrefs(d["offset"])
+
+    if crossrefs == []:
+        return []
+
+    ret = []
+
+    for crossref in crossrefs:
+        state = bin.angr.factory.full_init_state()
+        goal = crossref["from"]
+        simgr = bin.angr.factory.simulation_manager(state)
+        simgr.explore(find=goal)
+        for found in simgr.found:
+            rdi = found.solver.eval(found.regs.get("rdi"), cast_to=int)
+            rsi = found.solver.eval(found.regs.get("rsi"), cast_to=int)
+            rbp = found.solver.eval(found.regs.get("rbp"), cast_to=int)
+
+            rip_offset = rbp - rdi + 8
+            write_size = rsi
+
+            # Only store this vulnerability if we'll be able to write into rip completely
+            if write_size >= rip_offset + 8:
+                ret.append(StackBufferOverflow(crossref["from"], rip_offset, rsi))
+
+    return ret
+
+
 def find_vulns(bin: Binary) -> list[Vulnerability]:
     ret = []
 
     ret += find_gets_vulns(bin)
+    ret += find_fgets_vulns(bin)
 
     return ret
