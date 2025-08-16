466caf231406e4f325c05248076c8f9d99247afc
bpaul <bpaul@bpaul.xyz>
2025-08-16 12:30:49 +1000

    Move crossreference state finding into its own method

--- binary.py
+++ binary.py
@@ -25,10 +25,7 @@ class Binary:
         self.r2.cmd("aaa")
         self.afl = json.loads(self.r2.cmd("aflj"))
 
-        self.angr = angr.Project(
-            path,
-            auto_load_libs=False
-        )
+        self.angr = angr.Project(path, auto_load_libs=False)
 
     def crossrefs(self, symbol: int):
         return json.loads(self.r2.cmd(f"axtj {symbol}"))
@@ -36,3 +33,26 @@ class Binary:
     def load_string(self, addr: int) -> str:
         self.r2.cmd(f"s {addr}")
         return self.r2.cmd("ps")
+
+    # What's the type of state?!??!?!?!??!?!!?!?!!!?!?
+    def crossref_states(self, symbol: int, state) -> list[tuple]:
+        """
+        Get a list of angr states that reach a crossreference to a symbol call, starting at the given (angr) state
+        """
+        fn = next((fn for fn in self.afl if fn["name"] == symbol), None)
+        if fn is None:
+            return []
+
+        crossrefs = self.crossrefs(fn["offset"])
+        if crossrefs == []:
+            return []
+
+        d = []
+        for crossref in crossrefs:
+            print(crossref)
+            goal = crossref["from"]
+            simgr = self.angr.factory.simulation_manager(state)
+            simgr.explore(find=goal)
+            d += [(crossref, found) for found in simgr.found]
+
+        return d
--- vulns.py
+++ vulns.py
@@ -2,9 +2,6 @@ from binary import Binary
 from dataclasses import dataclass
 from typing import Optional
 
-import angr
-import claripy
-
 
 class Vulnerability:
     """Abstract class for potentially usable vulnerabilities"""
@@ -20,31 +17,18 @@ class StackBufferOverflow(Vulnerability):
 def find_gets_vulns(bin: Binary) -> list[Vulnerability]:
     """Find calls to gets into a stack buffer"""
 
-    crossrefs = []
-    for d in bin.afl:
-        if d["name"] == "sym.imp.gets":
-            crossrefs = bin.crossrefs(d["offset"])
-
-    if crossrefs == []:
-        return []
-
     ret = []
 
-    for crossref in crossrefs:
-        state = bin.angr.factory.full_init_state()
-        goal = crossref["from"]
-        simgr = bin.angr.factory.simulation_manager(state)
-        simgr.explore(find=goal)
-        for found in simgr.found:
-            rdi = found.solver.eval(found.regs.rdi, cast_to=int)
-            rbp = found.solver.eval(found.regs.rbp, cast_to=int)
+    for crossref, found in bin.crossref_states("sym.imp.gets", bin.angr.factory.full_init_state()):
+        rdi = found.solver.eval(found.regs.rdi, cast_to=int)
+        rbp = found.solver.eval(found.regs.rbp, cast_to=int)
 
-            rip_offset = rbp - rdi + 8
+        rip_offset = rbp - rdi + 8
 
-            # rdi-rbp is the number of bytes to write from the buffer to the end of the stack frame,
-            # 8 past that is the return address
+        # rdi-rbp is the number of bytes to write from the buffer to the end of the stack frame,
+        # 8 past that is the return address
 
-            ret.append(StackBufferOverflow(crossref["from"], rip_offset, None))
+        ret.append(StackBufferOverflow(crossref["from"], rip_offset, None))
 
     return ret
 
@@ -52,32 +36,19 @@ def find_gets_vulns(bin: Binary) -> list[Vulnerability]:
 def find_fgets_vulns(bin: Binary) -> list[Vulnerability]:
     """Find calls to fgets into a stack buffer which writes past the buffer size"""
 
-    crossrefs = []
-    for d in bin.afl:
-        if d["name"] == "sym.imp.fgets":
-            crossrefs = bin.crossrefs(d["offset"])
-
-    if crossrefs == []:
-        return []
-
     ret = []
 
-    for crossref in crossrefs:
-        state = bin.angr.factory.full_init_state()
-        goal = crossref["from"]
-        simgr = bin.angr.factory.simulation_manager(state)
-        simgr.explore(find=goal)
-        for found in simgr.found:
-            rdi = found.solver.eval(found.regs.rdi, cast_to=int)
-            rsi = found.solver.eval(found.regs.rsi, cast_to=int)
-            rbp = found.solver.eval(found.regs.rbp, cast_to=int)
+    for crossref, found in bin.crossref_states("sym.imp.fgets", bin.angr.factory.full_init_state()):
+        rdi = found.solver.eval(found.regs.rdi, cast_to=int)
+        rsi = found.solver.eval(found.regs.rsi, cast_to=int)
+        rbp = found.solver.eval(found.regs.rbp, cast_to=int)
 
-            rip_offset = rbp - rdi + 8
-            write_size = rsi
+        rip_offset = rbp - rdi + 8
+        write_size = rsi
 
-            # Only store this vulnerability if we'll be able to write into rip completely
-            if write_size >= rip_offset + 8:
-                ret.append(StackBufferOverflow(crossref["from"], rip_offset, rsi))
+        # Only store this vulnerability if we'll be able to write into rip completely
+        if write_size >= rip_offset + 8:
+            ret.append(StackBufferOverflow(crossref["from"], rip_offset, rsi))
 
     return ret
 
