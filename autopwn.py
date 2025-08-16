6adc27ac98f5b1b5372e9852a3cb63a3446d9f68
Max Guppy <theonly@mrcat.au>
2025-08-16 10:18:45 +1000

    Add basic argparse parser

--- autopwn.py
+++ autopwn.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 
 from binary import Binary
@@ -5,7 +6,11 @@ from goals import find_goals
 from vulns import find_vulns
 
 def main():
-    binary = Binary(sys.argv[1])
+    parser = argparse.ArgumentParser(description='Find and exploit vulnerabilities in a binary')
+    parser.add_argument("path", nargs=1)
+    args = parser.parse_args()
+
+    binary = Binary(args.path)
 
     goals = find_goals(binary)
     vulns = find_vulns(binary)
