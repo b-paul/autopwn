8877e5f5a5f97cee6d952d556166ce184fa2cc63
bpaul <bpaul@bpaul.xyz>
2025-08-16 10:21:05 +1000

    checksec

--- binary.py
+++ binary.py
@@ -1,5 +1,15 @@
 import cle
+from pwn import ELF
 
 class Binary():
     def __init__(self, path: str):
+        self.path = path
+
         self.loader = cle.Loader(path)
+
+        elf = ELF(path)
+
+        self.relro = elf.relro
+        self.canary = elf.canary
+        self.nx = elf.nx
+        self.pie = elf.pie
