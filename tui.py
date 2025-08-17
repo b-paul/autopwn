3f149960ba415bc22587b13fe3b0cad5fa16a10f
Max Guppy <theonly@mrcat.au>
2025-08-17 11:32:25 +1000

    Fiddle with tui layout

--- tui.py
+++ tui.py
@@ -1,19 +1,19 @@
 import asyncio
 from textual.app import App, ComposeResult
-from textual.containers import Grid
-from textual.widgets import Header, Footer, Log, Label, Markdown, ListView, ListItem
+from textual.containers import Horizontal, Vertical
+from textual.widgets import Header, Footer, Log, Label, Markdown, ListView, ListItem, Collapsible
 
 from binary import Binary
 from goals import Goal, WinFunction, SystemFunction, find_goals
 from vulns import Vulnerability, StackBufferOverflow, WinFunctionCall, UnconstrainedPrintf, BufferWrite, find_vulns
 
 class SecurityFeatures(Markdown):
-    def __init__(self, binary: Binary) -> None:
+    def __init__(self, binary: Binary, *args, **kwargs) -> None:
         super().__init__(f"""- RELRO: {binary.relro}
 - PIE: {binary.pie}
 - Canaries: {binary.canary}
 - NX: {binary.nx}
-""")
+""", *args, **kwargs)
         self.border_title = "Security Features"
 
 
@@ -31,10 +31,8 @@ class GoalItem(ListItem):
 
 
 class Goals(ListView):
-    def __init__(self, goals: list[Goal]) -> None:
-        super().__init__(*[
-            GoalItem(goal) for goal in goals
-        ])
+    def __init__(self, goals: list[Goal], *args, **kwargs) -> None:
+        super().__init__(*[GoalItem(goal) for goal in goals], *args, **kwargs)
         self.border_title = "Found goals"
 
 
@@ -42,25 +40,30 @@ class VulnItem(ListItem):
     def __init__(self, vuln: Vulnerability) -> None:
         super().__init__()
         self.vuln = vuln
-        self.border_title = "Found vulnerabilities"
 
     def compose(self) -> ComposeResult:
         match self.vuln:
             case StackBufferOverflow():
-                yield Label(f"Stack Buffer Overflow @ {self.vuln.addr:x} (rip offset: {self.vuln.saved_rip_offset:x}, max write size: {self.vuln.max_write_size})")
+                with Collapsible(title=f"Stack Buffer Overflow @ 0x{self.vuln.addr:x}"):
+                    yield Label(f"rip offset: {self.vuln.saved_rip_offset:x}")
+                    yield Label(f"Max write size: {self.vuln.max_write_size}")
             case WinFunctionCall():
-                yield Label(f"Win Function Call: {self.vuln.name}")
+                with Collapsible(title=f"Win Function Call @ ???"):
+                    yield Label(f"Win function: {self.vuln.name}")
             case UnconstrainedPrintf():
-                yield Label(f"Unconstrained printf() call @ {self.vuln.addr:x}")
+                with Collapsible(title=f"Unconstrained printf() call @ 0x{self.vuln.addr:x}"):
+                    pass
             case BufferWrite():
-                yield Label(f"Buffer write @ {self.vuln.instruction_addr:x} (type: {self.vuln.buffer_type}, addr: {self.vuln.buffer_addr:x}, len: {self.vuln.buffer_len})")
+                with Collapsible(title=f"Buffer write @ 0x{self.vuln.instruction_addr:x}"):
+                    yield Label(f"Buffer type: {self.vuln.buffer_type}")
+                    yield Label(f"Buffer address: 0x{self.vuln.buffer_addr:x}")
+                    yield Label(f"Buffer length: {self.vuln.buffer_len}")
             
 
 class Vulns(ListView):
-    def __init__(self, vulns: list[Vulnerability]) -> None:
-        super().__init__(*[
-            VulnItem(vuln) for vuln in vulns
-        ])
+    def __init__(self, vulns: list[Vulnerability], *args, **kwargs) -> None:
+        super().__init__(*[VulnItem(vuln) for vuln in vulns], *args, **kwargs)
+        self.border_title = "Found vulnerabilities"
 
 
 class Autopwn(App):
@@ -72,7 +75,9 @@ class Autopwn(App):
 
     def compose(self) -> ComposeResult:
         yield Header()
-        yield Grid(id="content")
+        with Vertical(id="content"):
+            yield Horizontal(id="info")
+            yield Horizontal(id="exploits")
         yield Log(id="log")
         yield Footer()
 
@@ -85,12 +90,12 @@ class Autopwn(App):
         self.query_one("#log").write_line("Loaded!")
 
         self.query_one("#content").border_title = f"Binary: {self.binary.path}"
-        self.query_one("#content").mount(SecurityFeatures(self.binary))
+        self.query_one("#info").mount(SecurityFeatures(self.binary, id="security"))
 
         self.query_one("#log").write_line("Finding goals...")
         self.goals = await asyncio.to_thread(lambda: find_goals(self.binary))
-        self.query_one("#content").mount(Goals(self.goals))
+        self.query_one("#exploits").mount(Goals(self.goals, id="goals"))
 
         self.query_one("#log").write_line("Finding vulnerabilities...")
         self.vulns = await asyncio.to_thread(lambda: find_vulns(self.binary, self.goals))
-        self.query_one("#content").mount(Vulns(self.vulns))
+        self.query_one("#exploits").mount(Vulns(self.vulns, id="vulns"))
