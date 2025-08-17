91e61fd3824ffc9d79754f3986099f6648bc69a2
stackotter <stackotter@stackotter.dev>
2025-08-17 13:16:12 +1000

    Add option to specify remote server to attack

--- autopwn.py
+++ autopwn.py
@@ -4,17 +4,31 @@ import logging
 from binary import Binary
 from goals import find_goals
 from vulns import find_vulns
-from exploit import exploit, ExploitError
+from exploit import exploit, ExploitError, Remote
 from tui import Autopwn
 
 def main():
     parser = argparse.ArgumentParser(description="Find and exploit vulnerabilities in a binary")
     parser.add_argument("path", nargs=1)
+    parser.add_argument("remote", nargs="?", default=None)
     parser.add_argument("-v", "--verbose", action="store_true", help="Print extra debug information")
     parser.add_argument("-i", "--interactive", action="store_true", help="Enable the pwn TUI")
     args = parser.parse_args()
 
     logging.getLogger("angr").setLevel(999)
+
+    remote = None
+    if args.remote is not None:
+        parts = args.remote.split(":")
+        if len(parts) != 2:
+            print(f"Invalid remote host string '{args.remote}'")
+            exit(1)
+        try:
+            port = int(parts[1])
+        except Exception:
+            print(f"Invalid port '{parts[1]}'")
+            exit(1)
+        remote = Remote(parts[0], port)
 
     if args.interactive:
         app = Autopwn(args)
@@ -30,7 +44,7 @@ def main():
             print(vulns)
 
         try:
-            output = exploit(binary, goals, vulns)
+            output = exploit(binary, goals, vulns, remote)
             if output is not None:
                 print(f"Flag: {output.decode()}")
         except ExploitError as e:
